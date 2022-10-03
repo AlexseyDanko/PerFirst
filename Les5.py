5130a08349b319d01b69c11d6554e9e1067615f1
AlexseyDanko <nopanicfear@yandex.ru>
2022-10-03 13:34:23 +0300

    create my first per

---
+++ Les5.py
@@ -0,0 +1,28 @@
+1  # int
+1.4567  # float
+"str1"  # str
+True  # bool
+False  # bool
+None  # zero
+122  #
+111  #
+3.28462  #
+73.872  #
+"take from queue"
+"put in a queue"
+per1 = "Hello Michail"
+per2 = "Good Day"
+per3 = "Good Work"
+per4 = 50512
+per5 = 58657
+per6 = 63299
+per7 = 21.523
+per8 = 32.2011
+per9 = 44.698
+per10 = None
+per11 = None
+per12 = None
+per13 = True
+per14 = False
+per15 = True
+
