4550eecd55a5d8823dbbfe2dde3ab5d7a2bad76f
AlexseyDanko <nopanicfear@yandex.ru>
2022-10-06 13:51:02 +0300

    create my first number files

--- Les5.py
+++ Les5.py
@@ -1,28 +1,28 @@
-1  # int
-1.4567  # float
-"str1"  # str
-True  # bool
-False  # bool
-None  # zero
-122  #
-111  #
-3.28462  #
-73.872  #
-"take from queue"
-"put in a queue"
-per1 = "Hello Michail"
-per2 = "Good Day"
-per3 = "Good Work"
-per4 = 50512
-per5 = 58657
-per6 = 63299
-per7 = 21.523
-per8 = 32.2011
-per9 = 44.698
-per10 = None
-per11 = None
-per12 = None
-per13 = True
-per14 = False
-per15 = True
+import math
+print(1**100) #**
+print(2**16) #**
+print(2-1000) #-
+print(2+2*2) #+*
+print(60/3) #/
+print(math.sqrt(144))
+print(2+5*14) #+*
+a = 2
+b = 5*14
+x = 2+5*14
+print(b)
+print(x)
+a1 = 66
+c = 11
+x1 = a1 - c
+print(x1)
+print("x=" + str(2+5*14))
+print("x=" + str(66-11))
+("math.sqrtx=" + str(16-8))
+b2 = 8
+c2 = 16
+a2 = (c2 - b2)**2
+print(a2)
+
+
+
 
