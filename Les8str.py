62d3c440642ef23030e06e35650ffffaf19a868d
AlexseyDanko <nopanicfear@yandex.ru>
2022-10-08 11:40:15 +0300

    string lesson

---
+++ Les8str.py
@@ -0,0 +1,29 @@
+str1 = 'Hi'
+str2 = "Buy"
+str3 = '"Way"'
+print(str1 + str2 + str3)
+per1 = 'Tools'
+per2 = per1 * 10
+print(per2)
+chislo1 = 100
+print(type(chislo1))
+chislo1 = str(chislo1)
+print(type(chislo1))
+chislo2 = 345.543
+print(type(chislo2))
+chislo2 = str(chislo2)
+print(type(chislo2))
+per3 = True
+print(type(per3))
+per3 = str(per3)
+print(type(per3))
+per4 = None
+print(type(per4))
+per4 = str(per4)
+print(type(per4))
+import math
+per5 = ((math.sqrt(144)+math.factorial(1)) - (math.sqrt(64)+10))
+print(per5)
+print(type(per5))
+per5 = str(per5)
+print(type(per5))
