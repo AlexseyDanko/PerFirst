071b0067bb3e3629799b3b58e627defae03d732f
AlexseyDanko <nopanicfear@yandex.ru>
2022-10-17 21:12:58 +0300

    1variant

---
+++ HomeWork1.py
@@ -0,0 +1,9 @@
+year_old_Anna = int(input("Введите возраст Anna"))
+year_old_Alex = int(input("Введите возраст Alex"))
+if year_old_Anna > year_old_Alex:
+    print("Anna старше")
+elif year_old_Anna == year_old_Alex:
+    print("Они одного возраста")
+else:
+    print("Alex старше")
+
